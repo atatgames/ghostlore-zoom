a633052bb4b379ed8ed0083699d366b8d646c016
atatgames <adam@ghostlore.sg>
2022-04-17 21:25:37 +0800

    first commit

---
+++ GhostloreExampleMod/ModLoader.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace GhostloreExampleMod
+{
+    /// <summary>
+    /// Entry point for your mod. In this example we just display some text on the screen, but anything is possible.
+    /// If you are new to modding for Unity games, there are many guides available online for other Unity games like Rimworld, Kerbal Space Program, and Cities Skylines.
+    /// </summary>
+    public class ModLoader : IModLoader
+    {
+        /// <summary>
+        /// Called when mod is first loaded.
+        /// </summary>
+        public void OnCreated()
+        {
+        }
+
+        /// <summary>
+        /// Called when mod is unloaded.
+        /// </summary>
+        public void OnReleased()
+        {
+        }
+
+        /// <summary>
+        /// Called when game is loaded.
+        /// </summary>
+        /// <param name="mode">Either a new game, or a previously saved game.</param>
+        public void OnGameLoaded(LoadMode mode)
+        {
+            var cam = Object.FindObjectOfType<PixelPerfectCamera>();
+            if (cam != null)
+            {
+                cam.refResolutionX = 960;
+                cam.refResolutionY = 540;
+            }
+        }
+
+        /// <summary>
+        /// Called when game is unloaded.
+        /// </summary>
+        public void OnGameUnloaded()
+        {
+            var cam = Object.FindObjectOfType<PixelPerfectCamera>();
+            if (cam != null)
+            {
+                cam.refResolutionX = 640;
+                cam.refResolutionY = 360;
+            }
+        }
+    }
+}
